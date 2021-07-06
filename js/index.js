d83ab2812d7c8847f38acc5dd8d82968f2a2cfc7
naru92 <skfn8000@naver.com>
2021-07-06 12:59:56 +0900

    mainpage update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
-  
 
 
+$(function(){
+ var $hover = $('#leftmenuToggle')
+ var $leftmenu = $('#leftmenu')
+    $hover.hover(function(){
+        $(this).addClass('leftmenuToggle')
+        $(leftmenu).addClass('hoverEvent');
+
+    })
+
+})
+
+
+// $(window).scroll(function() {
+//     var scrollTop = $(this).scrollTop();
+//
+//     if ( scrollTop < 100 ) {
+//         scrollTop = 0;
+//     }
+//     else if ( scrollTop > 9600 ) {
+//         scrollTop = 0;
+//     }
+//
+//     var duration = 1000;
+//     $('.maginContainer > .quick').stop().animate({top:scrollTop}, duration);
+//
+//     console.log(scrollTop);
+// });
 
 
  /*네비게이션*/
